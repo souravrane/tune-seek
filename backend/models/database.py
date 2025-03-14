bf9063f31768cee67fd5e7b6d27e705b6ddc69e3
Sourav Mohan Rane <SouravMohan.Rane@wisetechglobal.com>
2025-03-15 01:05:18 +0530

    database connection

--- backend/models/database.py
+++ backend/models/database.py
@@ -6,9 +6,13 @@ DB_NAME = "tune_seek"
 client = MongoClient(DB_URI)
 db = client[DB_NAME]
 
+def connect_db():
+    print(f"Connected to MongoDB : {DB_URI}")
+
 def store_fingerprint(title, artist, hashes):
     db.songs.insert_one({
         "title": title,
         "artist": artist,
         "hashes": hashes
     })
+ 
