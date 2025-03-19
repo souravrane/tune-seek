59188f86ad5cb5ac24f6701396685fc1746a2fd8
souravrane <souravrane112@gmail.com>
2025-03-20 00:46:38 +0530

    updated the routes

--- backend/routes/songs.py
+++ backend/routes/songs.py
@@ -13,7 +13,7 @@ async def get_health():
     return {"message : song route is healthy."}
 
 @router.post("/upload")
-async def upload_song(file: UploadFile = File(...), title: str = Form("title"), artist: str = Form("artist")):
+async def upload_song(file: UploadFile = File(...), title: str = Form("title"), artist: str = Form("artist"), url: str = Form("url"), image: str = Form("image")):
     audio_path = os.path.join(UPLOADS_DIR, file.filename)
 
     #save file in the folder
@@ -25,7 +25,7 @@ async def upload_song(file: UploadFile = File(...), title: str = Form("title"), 
     hashes = create_hashes(fingerprint)
     
     # store in DB
-    store_fingerprint(title, artist, hashes)
+    store_fingerprint(name = title, artist = artist, hashes = hashes, url = url, image = image)
 
     return {"message": f"{title} song added"}
 
@@ -37,9 +37,9 @@ async def match_audio(file: UploadFile = File(...)):
     with open(audio_path, "wb") as buffer:
         buffer.write(await file.read())
 
-    # Extract fingerprints from query audio (only first 30 seconds)
+    # Extract fingerprints from query audio (only first 'duration' seconds)
     fingerprint = extract_fingerprint(audio_path, duration=20)
     audio_hashes = create_hashes(fingerprint)
     
-    matches = match_fingerprint(audio_hashes)
-    return {"matching songs " : matches.__str__()}
+    matching_tune = match_fingerprint(audio_hashes)
+    return {"matching_tune" : matching_tune}
